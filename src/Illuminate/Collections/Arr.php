26020ca158fc0d3ff2560712798368bf6fdee2f0
Elminson De Oleo Baez <edeoleo@gmail.com>
2023-12-04 09:00:37 -0500

    Refactor array permutations logic to use array_fill and array_merge

--- src/Illuminate/Collections/Arr.php
+++ src/Illuminate/Collections/Arr.php
@@ -910,10 +910,7 @@ class Arr
 		$permutations = $array;
 		$size = count($array);
 
-		$indices = [];
-		for ($index = 0; $index < $size; $index++) {
-			$indices[$index] = 0;
-		}
+		$indices = array_fill(0, $size, 0);
 
 		while (1) {
 
@@ -921,19 +918,21 @@ class Arr
 			for ($index = 0; $index < $size; $index++) {
 				$res[] = $array[$index][$indices[$index]];
 			}
+
 			$permutations[] = $res;
 			$next = $size - 1;
+
 			while ($next >= 0 &&
 				   ($indices[$next] + 1 >= count($array[$next]))) {
 				$next--;
 			}
+
 			if ($next < 0) {
 				return $permutations;
 			}
+
 			$indices[$next]++;
-			for ($index = $next + 1; $index < $size; $index++) {
-				$indices[$index] = 0;
-			}
+			$indices = array_merge(array_slice($indices, 0, $next + 1), array_fill($next + 1, $size - $next - 1, 0));
 
 		}
 
